af1f9105f8d7772e8f41ed0057b659900c326631
Greg O'Neill <gioneill@gmail.com>
2019-03-08 15:42:27 -0500

    Correct init method to be used from IB.

--- MinitexPDFProtocols/Book Reader/BookViewController.swift
+++ MinitexPDFProtocols/Book Reader/BookViewController.swift
@@ -24,7 +24,7 @@ class BookViewController: UIViewController, MinitexPDFViewController, UIPopoverP
     }
 
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
 
     var pdfDocument: PDFDocument?
